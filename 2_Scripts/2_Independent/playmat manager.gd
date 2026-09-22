61ae292fe5bc6441f9cec26217d5c66c5a5f5bc5
Alan Richardson <monkeyboysmash23@gmail.com>
2026-09-22 12:28:40 -0700

    added page flip & trait sheet resource

--- 2_Scripts/2_Independent/playmat manager.gd
+++ 2_Scripts/2_Independent/playmat manager.gd
@@ -5,21 +5,29 @@ class_name PlaymatManager
 @export_category("References")
 @export var paperHolder : Control
 
+@export_category("Delete Later (For testing)")
+@export var mainTraitSheet : PaperInfo
+
 var testPaperRef : PackedScene = load("res://1_Scenes/1_Objects/paper.tscn") #TODO: Make actual papers, replace variable
 
 var papers : Array[Paper]
 
 func _ready() -> void:
 	await get_tree().process_frame
-	_create_paper(testPaperRef, Color("4b3d44"))
-	_create_paper(testPaperRef, Color("4b3d44"))
-	_create_paper(testPaperRef, Color("4b3d44"))
+	_create_paper(testPaperRef, Color("4b3d44"), mainTraitSheet)
+	#_create_paper(testPaperRef, Color("4b3d44"))
+	#_create_paper(testPaperRef, Color("4b3d44"))
 
 
 #-----------------------
 
-func _create_paper(paperRef : PackedScene, paperColor : Color):
+func _create_paper(paperRef : PackedScene, paperColor : Color, paperInfo : PaperInfo):
 	var paperInstance : Paper = paperRef.instantiate()
+
+	if paperInfo.info.size() > 1:
+		pass #TODO: Packet Visual
+
+	paperInstance._setup_paper(paperInfo)
 	
 	paperInstance.self_modulate = paperColor
 	paperInstance.playmat = self
